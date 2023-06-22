d065c3dd6f2e3306f4e04ed9aadd00c60cf88091
Bhoomi Shakya <94596019+BhoomiShakya@users.noreply.github.com>
2023-06-22 17:23:47 +0530

    Create Number_Complement.java

---
+++ Number_Complement.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findComplement(int n) {
+        String s=Integer.toBinaryString(n);
+        //System.out.println(s);
+        String ans="";
+        for(char ch :s.toCharArray()){
+            if(ch=='1'){
+                ans+="0";
+            }
+            else{
+                ans+="1";
+            }
+        }
+        return Integer.parseInt(ans,2);
+    }
+}
