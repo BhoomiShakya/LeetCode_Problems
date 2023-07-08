bba876cd0e41dc51d114979ce288de85b4a5697e
Bhoomi Shakya <94596019+BhoomiShakya@users.noreply.github.com>
2023-07-08 19:27:52 +0530

    Create LongestCommonPrefix.java

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String longestCommonPrefix(String[] s) {
+        Arrays.sort(s);
+        String s1=s[0];
+        String s2=s[s.length-1];
+        int i=0;
+        while(i<s1.length()&& i<s2.length()){
+            if(s1.charAt(i) == s2.charAt(i)){
+                   i++;
+            }
+            else{
+                break;
+            }
+         
+        }
+        return (s2.substring(0,i));
+    }
+}
