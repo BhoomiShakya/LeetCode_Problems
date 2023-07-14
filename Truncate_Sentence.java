9bdd6f7ca16eea41a162646cb77e9235e4f5d089
Bhoomi Shakya <94596019+BhoomiShakya@users.noreply.github.com>
2023-07-14 18:12:05 +0530

    Create  Truncate_Sentence.java

---
+++ Truncate_Sentence.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        return solution(s,k);
+    }
+    public static String solution(String s, int k){
+        String arr[]= s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<k;i++){
+            if(i<arr.length){
+                sb.append(arr[i]).append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+}
